4098784e367005ff6faa34add10a75be9a81d5da
Faaz Mohamed <fuzzinc45@gmail.com>
2025-12-23 17:22:48 -0500

    Refactor Firebase admin initialization to use environment variables for credentials

--- server.js
+++ server.js
@@ -16,8 +16,13 @@ app.use(express.json());
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 admin.initializeApp({
-  credential: admin.credential.cert(require("./serviceAccount.json")),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  }),
 });
+
 
 const db = admin.firestore();
 
